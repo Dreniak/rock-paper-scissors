884bb66848b38d81101914bef511424d2a040e5d
Michel Wesley <michelwesley2016@gmail.com>
2018-07-14 00:23:06 -0300

    Set game logic to work with buttons

--- script.js
+++ script.js
@@ -1,57 +1,70 @@
-/*var scorePlayer = 0;
+var scorePlayer = 0;
 var scoreComputer = 0;
+var round = 0;
 var choices = ['rock', 'paper', 'scissors'];
+var buttons = document.querySelectorAll('button');
 
-function computerPlay () {
-  var rdnNum = Math.floor(Math.random() * 3 + 1); // 1R, 2P, 3S
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    var clickedButton = e.target.id;
+    var clickedButtonNumber = playerPlay(clickedButton);
+    console.log(round + 1);
+    console.log(playRound(clickedButtonNumber, computerPlay())); // calls playRound function
+    round++;
+    console.log(scorePlayer, scoreComputer);
+    if (round == 5) {
+      console.log(calcMatchWinner());
+      scorePlayer = 0;
+      scoreComputer = 0;
+      round = 0;
+    }
+  });
+});
+
+function computerPlay () { // generates a random move for computer
+  var rdnNum = Math.floor(Math.random() * 3 + 1);
   return rdnNum;
 }
 
-function playerPlay () {
-  var choice = prompt("Rock, paper, scissors!?");
-  if (choice == undefined) {return "dksnkn";}
-  var n = 0;
-  while (n < 3) {
-    if (choice.toLowerCase() == choices[n]) {
+function playerPlay (choice) { // tranform string (choice) into number
+  for (n = 0; n < choices.length; n++) {
+    if (choice == choices[n]) {
       return n + 1;
-    } else {n++;}
+      break;
+    }
   }
 }
 
 function playRound (playerSelection, computerSelection) {
-  console.log(`Player move: ${choices[playerSelection - 1]} || Machine move: ${choices[computerSelection - 1]}`);
-  switch (true) {
-    case playerSelection > computerSelection && !(playerSelection == 3 && computerSelection == 1) || playerSelection == 1 && computerSelection == 3:
-      scorePlayer++;
-      return (`You win! ${choices[playerSelection - 1]} beats ${choices[computerSelection - 1]}`);
+  var playerIndex = playerSelection - 1;
+  var computerIndex = computerSelection - 1;
+
+  var playerWinCondition = playerSelection > computerSelection && !(playerSelection == 3 && computerSelection == 1) || playerSelection == 1 && computerSelection == 3;
+  var computerWinCondition = playerSelection < computerSelection && !(playerSelection == 1 && computerSelection == 3) || playerSelection == 3 && computerSelection == 1;
+
+  console.log(`Player move: ${choices[playerIndex]} || Computer move: ${choices[computerIndex]}`);
+
+  switch (true) { // Check moves from players and determines a winner
+    case playerWinCondition:
+      scorePlayer++
+      return (`You win! ${choices[playerIndex]} beats ${choices[computerIndex]}`);
     break;
-    case playerSelection < computerSelection && !(playerSelection == 1 && computerSelection == 3) || playerSelection == 3 && computerSelection == 1:
-      scoreComputer++;
-      return (`You lose! ${choices[computerSelection - 1]} beats ${choices[playerSelection - 1]}`);
+
+    case computerWinCondition:
+      scoreComputer++
+      return (`You lose! ${choices[computerIndex]} beats ${choices[playerIndex]}`);
     break;
-    case playerSelection == computerSelection:
-      return ("It's a tie!");
-    break;
+
     default:
-      return "Insert a valid move to play! Restarting the match...";
+      return "It's a tie!";
   }
 }
 
-function game () {
-  for (n = 0; n < 5; n++) {
-    console.log(`${n + 1}st Round:`)
-    var result = playRound(playerPlay(), computerPlay());
-    console.log(result);
-    if (result == "Insert a valid move to play! Restarting the match...") {
-      n--;
-    }
-  }
-  console.log(scorePlayer, scoreComputer);
+function calcMatchWinner () {
+  console.log("Final: " + scorePlayer, scoreComputer);
   if (scorePlayer > scoreComputer) {
     return "Congrats! You won the 5 rounds match!";
   } else if (scorePlayer < scoreComputer) {
     return "Too bad! you lost the 5 rounds match, wanna try again?"
   } else {return "So close! It's a tie!";}
 }
-
-console.log(game())
