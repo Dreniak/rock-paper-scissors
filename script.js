c0bdc4ce09fa5cb74047a0328c8cfcb7c1c7ac23
Michel Wesley <michelwesley2016@gmail.com>
2018-07-16 10:26:14 -0300

    set game match winner screen and round counter

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ var buttons = document.querySelectorAll('.buttons button');
 var consoleArea = document.querySelector(".console");
 var roundWinnerBox = document.querySelector(".round-winner-box");
 var roundWinner = document.querySelector("#round-winner");
-var matchResult = document.querySelector("#match-result")
+var roundResult = document.querySelector("#round-result")
 var nextButton = document.querySelector("#next");
+var matchWinnerBox = document.querySelector(".match-winner-box");
+var matchWinner = document.querySelector("#match-winner");
+var matchResult = document.querySelector("#match-result");
+var restartButton = document.querySelector("#restart");
 var playerMove = document.querySelector("#player-move");
 var computerMove = document.querySelector("#computer-move");
 var waitTime = document.querySelector("#wait-time");
 var playerScore = document.querySelector("#player-score");
 var computerScore = document.querySelector("#computer-score");
+var roundNumber = document.querySelector(".round p span");
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -21,32 +26,33 @@ buttons.forEach((button) => {
     var computerChoiceNumber = computerPlay();
     var computerChoice = choices[computerChoiceNumber-1];
 
-    buttons.forEach((button) => {button.disabled=true});
+    buttons.forEach((button) => { button.disabled=true;});
 
     displayMoves(playerChoice, computerChoice);
     //displays the chosen moves in the hud
     setTimeout(() => {
-      console.log(round + 1);
     //Makes code waits till displayMoves() finishes
       playRound(playerChoiceNumber, computerChoiceNumber);
       // calls playRound function
       round++;
       displayScore();
       if (round == 5) {
-        console.log(calcMatchWinner());
-        scorePlayer = 0;
-        scoreComputer = 0;
-        round = 0;
+        calcMatchWinner();
+        resetDisplay();
+        setRoundWinnerScreen();
+        restartButton.addEventListener("click", () => {
+          matchWinnerBox.hidden = true;
+          consoleArea.hidden = false;
+          buttons.forEach((button) => {button.disabled=false});
+          scorePlayer = 0;
+          scoreComputer = 0;
+          round = 0;
+          roundNumber.textContent = `${round + 1}`;
+        });
         return;
       }
       resetDisplay();
-      consoleArea.hidden = true;
-      roundWinnerBox.hidden = false;
-      nextButton.addEventListener("click", () => {
-        consoleArea.hidden = false;
-        roundWinnerBox.hidden = true;
-        buttons.forEach((button) => {button.disabled=false});
-      });
+      setRoundWinnerScreen();
     }, 2500);
   });
 });
@@ -64,6 +70,22 @@ function playerPlay (choice) { // tranform string (choice) into number
     }
   }
 }
+
+function setRoundWinnerScreen () {
+  consoleArea.hidden = true;
+  roundWinnerBox.hidden = false;
+  nextButton.addEventListener("click", () => {
+    roundWinnerBox.hidden = true;
+    if (round != 5) {
+    consoleArea.hidden = false;
+    roundNumber.textContent = `${round + 1}`;
+    buttons.forEach((button) => {button.disabled=false});
+    } else {
+    matchWinnerBox.hidden = false;
+    }
+  });
+}
+
 
 function displayMoves (playerChoice, computerChoice) {
   playerMove.textContent = `You chose ${playerChoice}!`;
@@ -102,26 +124,30 @@ function playRound (playerSelection, computerSelection) {
     case playerWinCondition: //case player win
       scorePlayer++
       roundWinner.textContent = "PLAYER";
-      matchResult.textContent= `You won! ${choices[playerIndex]} beats ${choices[computerIndex]}!`.toUpperCase();
+      roundResult.textContent= `You won! ${choices[playerIndex]} beats ${choices[computerIndex]}!`.toUpperCase();
     break;
 
     case computerWinCondition: //case computer win
       roundWinner.textContent = "COMPUTER";
       scoreComputer++
-      matchResult.textContent = `You lose! ${choices[computerIndex]} beats ${choices[playerIndex]}`.toUpperCase();
+      roundResult.textContent = `You lose! ${choices[computerIndex]} beats ${choices[playerIndex]}`.toUpperCase();
     break;
 
     default:
       roundWinner.textContent = "IT'S A TIE";
-      matchResult.textContent = "";
+      roundResult.textContent = `It's pretty even, huh?`.toUpperCase();
   }
 }
 
 function calcMatchWinner () {
-  console.log("Final: " + scorePlayer, scoreComputer);
   if (scorePlayer > scoreComputer) {
-    return "Congrats! You won the 5 rounds match!";
+    matchWinner.textContent = "PLAYER!";
+    matchResult.textContent = "Congrats! You Won! Wanna try again?";
   } else if (scorePlayer < scoreComputer) {
-    return "Too bad! you lost the 5 rounds match..."
-  } else {return "So close! It's a tie!";}
+    matchWinner.textContent = "COMPUTER!";
+    matchResult.textContent = "Too bad! You lost! Maybe next time...";
+  } else {
+    matchWinner.textContent =  "IT'S A TIE!";
+    matchResult.textContent = "So close! Play again!";
+  }
 }
